954c9639412f5d42acbdabd897b91fbdab393316
UmeshBabu7 <umeshtamang129@gmail.com>
2026-04-22 07:47:57 +0545

    chore(api_urls): update API URL configuration

--- expensetracker/api_urls.py
+++ expensetracker/api_urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 api_urls = [
     path("api/auth/", include("accounts.urls")),
-    path("api/expenses/", include("expenses.urls")),
+    path("api/", include("expenses.urls")),
 ]
